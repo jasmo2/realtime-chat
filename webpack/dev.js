aef231b4d8319af549060ab7dcdd9faaf2f3812c
Jaime Molina <jasmo2.com@gmail.com>
2020-03-15 15:58:58 -0500

    Explanation (#2)

--- webpack/dev.js
+++ webpack/dev.js
@@ -11,14 +11,14 @@ module.exports = merge(commonConfig, {
     'react-hot-loader/patch',
     `webpack-dev-server/client?http://localhost:${PORT}`,
     'webpack/hot/only-dev-server',
-    './index.tsx' // the entry point of our app
+    './index.tsx' // <-
   ],
   devServer: {
-    hot: true // enable HMR on the server
+    hot: true
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ]
 })
